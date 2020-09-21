94e0729ebffb5d9fd6dec52b30e97f86fbb7fea4
Kyoji Kurata <kurata.kyoji.home@outlook.jp>
2020-09-21 19:33:30 +0900

    revise index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="col-12 col-md-9">
                 <?php if ( is_month() ) : ?>
-                    <h2 class="main_title"><?php wp_time('Y年m月'); ?></h2>
+                    <h2 class="main_title"><?php the_time('Y年m月'); ?></h2>
                 <?php else: ?>
                     <h2 class="main_title"><?php wp_title(''); ?></h2>
                 <?php endif; ?>
