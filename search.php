593b7d9372c794c24866c353c0a8a78bd2f25d2c
Kyoji Kurata <kurata.kyoji.home@outlook.jp>
2020-09-21 21:02:40 +0900

    create 404.php

--- search.php
+++ search.php
@@ -4,11 +4,11 @@
 
     <main class="main">
         <div class="container">
-            <h2 class="main_title">「<?php the_search_query(); ?>」</h2>
+            <h2 class="main_title">「<?php the_search_query(); ?>」の検索結果</h2>
             <div class="row">
 
                 <?php if ( have_posts() ) : ?>
-                    <?php while ( have_posts() ) : the_posts(); ?>
+                    <?php while ( have_posts() ) : the_post(); ?>
                         <div class="col-md-4">
                             <?php get_template_part('template-parts/loop', 'news'); ?>
                         </div>
