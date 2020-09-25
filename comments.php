479476c5906ab4313bf6a5ca76f46a36cc93f509
Kyoji Kurata <kurata.kyoji.home@outlook.jp>
2020-09-25 14:52:12 +0900

    revise comments.php

--- comments.php
+++ comments.php
@@ -1,6 +1,9 @@
 <section class="comments">
 <?php
-comment_form();
+$comment_form_args = array(
+    'title_reply' => 'コメント投稿フォーム',
+);
+comment_form( $comment_form_args );
 if ( have_comments() ) :
 ?>
     <p><?php comments_number(); ?></p>
