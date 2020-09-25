6acd5fae1ee797b43259106d2eb14e4aaccb434d
Kyoji Kurata <kurata.kyoji.home@outlook.jp>
2020-09-25 14:42:27 +0900

    revise single.php

--- single.php
+++ single.php
@@ -24,6 +24,8 @@
                         <div class="content">
                             <?php the_content(); ?>
                         </div>
+
+                        <?php comments_template(); ?>
                     </div>
 
                     <div class="postLinks">
