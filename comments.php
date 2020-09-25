f9ca3567f95760511636703e669fed3ec6e48f5b
Kyoji Kurata <kurata.kyoji.home@outlook.jp>
2020-09-25 15:26:07 +0900

    revise comments.php

--- comments.php
+++ comments.php
@@ -6,12 +6,21 @@ $comment_form_args = array(
 comment_form( $comment_form_args );
 if ( have_comments() ) :
 ?>
-    <p><?php comments_number(); ?></p>
+    <p><?php comments_number('コメントはありません。', 'コメントが1件あります。', 'コメントが%件あります。'); ?></p>
     <ol class="commentlist">
-        <?php wp_list_comments(); ?>
+        <?php 
+        $wp_list_comments_args = array(
+            'avatar_size' => '50'
+        );
+        wp_list_comments( $wp_list_comments_args );
+        ?>
     </ol>
 <?php
-paginate_comments_links();
+$paginate_comments_links _args = array(
+    'prev_text' => '←前のコメントページ',
+    'next_text' => '次のコメントページ→',
+);
+paginate_comments_links( $paginate_comments_links_args );
 endif;
 ?>
 </section>
